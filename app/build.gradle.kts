bf5c754d989524434e89073d8f61a366ab5fcb64
MelinaCora <melinaccora97@gmail.com>
2026-07-04 12:30:21 -0300

    cambio de version de firebase BoM

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
 
     // Firebase
-    implementation(platform("com.google.firebase:firebase-bom:34.15.0"))
+    implementation(platform("com.google.firebase:firebase-bom:33.9.0"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
 
